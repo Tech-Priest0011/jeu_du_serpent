8e80c473d5026cee6e79568e6a0de239ae7f357c
Jay <jbcave@hotmail.com>
2020-11-20 11:59:01 -0500

    Video terminer, serpent reussi

--- js/script.js
+++ js/script.js
@@ -20,13 +20,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
             this.pomme = new Pomme(this); //nouvelle pomme et reference
 
-            this.serpent = new Serpent(); //nouveau snek
+            this.serpent = new Serpent(this); //nouveau snek
         }
 
         finPartie(){
             if(this.pomme !== undefined){ //si il y a une pomme elle est supprimer
                 this.pomme.supprimerPomme();
                 this.pomme = undefined;
+            }
+            if(this.serpent !== undefined){
+                this.serpent.supprimerSerpent();
+                this.serpent = undefined;
             }
 
         }
@@ -38,8 +42,106 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     //Serpent
     class Serpent{
-        constructor() {
+        constructor(_leJeu) {
             console.log("Creation serpent");
+
+            this.leJeu = _leJeu;
+
+            this.currentX = -1;
+            this.currentY = 0;
+            this.nextX = 1;
+            this.nextY = 0;
+            this.serpentLongueur = 1;
+            this.tblCarreSerpent = [];
+            this.touche = false;
+            this.vitesse = 250;
+            this.timing = setInterval(this.controleSerpent.bind(this), this.vitesse);
+
+            document.addEventListener("keydown", this.appuiTouche.bind(this)); //lier a la class serpent
+        }
+
+        appuiTouche(_evt){
+            var evt = _evt;
+
+            //console.log(evt.keyCode);
+
+            this.deplacementSerpent(evt.keyCode); //obtien la touche
+        }
+
+        deplacementSerpent(directionCode){
+            switch (directionCode){
+                case 37: //fleche gauche
+                    this.nextX = -1;
+                    this.nextY = 0;
+                    break;
+                case 38: //fleche haut
+                    this.nextX = 0;
+                    this.nextY = -1;
+                    break;
+                case 39: //fleche droite
+                    this.nextX = 1;
+                    this.nextY = 0;
+                    break;
+                case 40: //fleche bas
+                    this.nextX = 0;
+                    this.nextY = 1;
+                    break;
+            }
+
+            //console.log(this.nextX, this.nextY);
+
+        }
+
+        controleSerpent(){
+            var prochainX = this.currentX + this.nextX;
+            var prochainY = this.currentY + this.nextY;
+
+            this.tblCarreSerpent.forEach(function(element){ //le serpent se touche
+                if(prochainX === element[1] && prochainY === element[2]){
+                    console.log("touche serpent");
+                    this.leJeu.finPartie();
+                    this.touche = true;
+                }
+            }.bind(this));
+
+            if(prochainY < 0 || prochainX < 0  || prochainY > this.leJeu.grille-1 || prochainX > this.leJeu.grille-1){
+                console.log("Limite");
+                this.leJeu.finPartie();
+                this.touche = true;
+            }
+
+            if(!this.touche){
+                if(this.currentX === this.leJeu.pomme.pomme[1] && this.currentY === this.leJeu.pomme.pomme[2]){
+                    this.serpentLongueur++;
+
+                    this.leJeu.affichagePointage(this.serpentLongueur);
+
+                    this.leJeu.pomme.supprimerPomme();
+                    this.leJeu.pomme.ajoutPomme();
+                }
+                this.dessinCarre(prochainX, prochainY);
+                this.currentX = prochainX;
+                this.currentY = prochainY;
+            }
+        }
+
+        dessinCarre(x, y){
+            var unCarre = [this.leJeu.s.rect(x * this.leJeu.carre, y * this.leJeu.carre, this.leJeu.carre, this.leJeu.carre), x, y]; //dessine le serpent
+            this.tblCarreSerpent.push(unCarre); //Mets les carres dans un tableau pour avoir la longueur
+
+            if(this.tblCarreSerpent.length > this.serpentLongueur){
+                this.tblCarreSerpent[0][0].remove(); //enleve un carre a l'ecran
+                this.tblCarreSerpent.shift(); //enleve du tableau
+            }
+        }
+
+        supprimerSerpent(){
+            clearInterval(this.timing); //arrete le timer
+
+            while (this.tblCarreSerpent.length > 0){ //supprime le serpent
+                this.tblCarreSerpent[0][0].remove();
+                this.tblCarreSerpent.shift();
+            }
         }
     }
 
